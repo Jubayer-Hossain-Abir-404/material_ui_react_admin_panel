c16378898953e8442b671465f8c674bb71f351be
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-01-19 07:28:09 +0600

    Update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./topbar.css"
-import { NotificationsNone } from '@material-ui/icons';
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
 
 export default function Topbar() {
     return (
@@ -18,6 +18,16 @@ export default function Topbar() {
                             2
                         </span>
                     </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">
+                            2
+                        </span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar"/>
                 </div>
             </div>
         </div>
